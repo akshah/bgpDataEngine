d22414a391a32b53a557aeb8a25bb6a78dbe1e59
Anant Shah <akshah@cs.colostate.edu>
2016-03-06 19:40:31 -0700

    Path update

--- sample/bdeFetchMRT.py
+++ sample/bdeFetchMRT.py
@@ -18,17 +18,5 @@ if __name__ == '__main__':
 
     #bde.getRange('updates','20140701','20140705',collectors=['route-views.jinx'])
 
-    Getting data from DB
-    day='20140705'
-    peer='196.223.14.10'
-    day='20140701'
-    #peer='195_66_224_114'
-    bde.getUpdateMessages(peer,day)
-    while True:
-        val=bde.messageQueue.get()
-        if not val:
-            break
-        print(val)
-
     end_time,_=currentTime()
     print('Finished processing in '+str(int((end_time-start_time)/60))+' minutes and '+str(int((end_time-start_time)%60))+' seconds.')
